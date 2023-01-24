208413b139f0cce35dbb704cad8413f473e4495a
nicoGherasim <nico.gherasim@wantsome.ro>
2023-01-24 21:05:10 +0200

    Implement missing methods

--- DatabasesPractice/DatabasesPractice/ItemsRepository.cs
+++ DatabasesPractice/DatabasesPractice/ItemsRepository.cs
@@ -10,8 +10,10 @@ namespace AdoNetPractice
             int numberOfItems = 0;
             using (SqlConnection conn = new SqlConnection(Constants.ConnectionString))
             {
-                //TO DO
                 // write the corresponding code to get the number of items
+                conn.Open();
+                SqlCommand readNumberOfItems = new SqlCommand("SELECT COUNT(*) FROM Items", conn);
+                numberOfItems = (int)readNumberOfItems.ExecuteScalar();
             }
             return numberOfItems;
         }
@@ -20,8 +22,24 @@ namespace AdoNetPractice
         {
             List<Item> items = new List<Item>();
 
-            //TO DO
             // write the corresponding code to get  all the items
+            using (SqlConnection conn = new SqlConnection(Constants.ConnectionString))
+            {
+                conn.Open();
+                SqlCommand readAllItems = new SqlCommand("SELECT * FROM Items", conn);
+                SqlDataReader allItems = readAllItems.ExecuteReader();
+
+                while (allItems.Read())
+                {
+                    var item = new Item
+                    {
+                        Id = (int)allItems["Id"],
+                        Name = (string)allItems["Name"],
+                        Price = (int)allItems["Price"],
+                    };
+                    items.Add(item);
+                }
+            }
 
             return items;
         }
--- DatabasesPractice/DatabasesPractice/OrdersRepository.cs
+++ DatabasesPractice/DatabasesPractice/OrdersRepository.cs
@@ -1,4 +1,5 @@
 ﻿using System.Data.SqlClient;
+using Uitls;
 
 namespace AdoNetPractice
 {
@@ -8,8 +9,22 @@ namespace AdoNetPractice
         {
             Order order = new Order();
 
-            //TO DO
-            // write the corresponding code to get the order by id
+            using (SqlConnection conn = new SqlConnection(Constants.ConnectionString))
+            {
+                conn.Open();
+                SqlCommand readOrders = new SqlCommand($"SELECT * FROM Orders WHERE Id={orderId}", conn);
+                SqlDataReader orders = readOrders.ExecuteReader();
+
+                while (orders.Read())
+                {
+                    order = new Order
+                    {
+                        Id = (int)orders["Id"],
+                        NumberOfPieces = (int)orders["NumberOfPieces"],
+                        ItemId = (int)orders["ItemId"],
+                    };
+                }
+            }
 
             return order;
         }
